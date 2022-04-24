82116015c142511d7ef7d1cee54138733a61941d
Carlos Roberto <carlos.rrds@gmail.com>
2022-04-24 00:31:11 -0300

    feat: criado GlobalStyled

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyles from "./Styles/global";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyles />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
